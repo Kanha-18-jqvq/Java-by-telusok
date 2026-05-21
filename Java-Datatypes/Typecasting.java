ee597aa8d246c1607cc96846d76fed1a15c1156b
Kanha-18-jqvq <biswaranjandas7108@gmail.com>
2026-05-21 11:10:08 +0530

    Type Casting

---
+++ Java-Datatypes/Typecasting.java
@@ -0,0 +1,12 @@
+// package Java-Datatypes;
+
+public class Typecasting {
+    public static void main(String[] args) {
+        int a=90;
+        float f=98.7f;
+        a=(int)f;//explicit Typecasting
+        f=a;//implicit typecasting
+        System.out.println("a ="+a);
+        System.out.println("f ="+f);
+    }
+}
